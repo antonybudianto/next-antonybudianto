81ff71d9b07eab05ddc24558d8d823583367fd2e
Antony <antony.budianto@tokopedia.com>
2021-06-01 18:08:02 +0700

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,110 @@
 import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="overflow-hidden flex justify-center items-center h-screen">
+    <div className="bg-gradient-to-br from-gray-900 via-gray-800 to-gray-800 text-white h-screen">
       <Head>
-        <title>Hello</title>
+        <title>Antony Budianto</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="px-4 sm:px-6 md:px-8">
-        <div className="">
-          <h1 className="text-4xl sm:text-6xl lg:text-7xl leading-none font-extrabold tracking-tight text-gray-900 mt-8 mb-8 sm:mt-14 sm:mb-10">
-            Welcome
-          </h1>
-          <p className="text-gray-500 text-lg sm:text-2xl sm:leading-10 font-medium mb-10 sm:mb-11">
-            Starter for any use case
+      <div className="container mx-auto px-4 sm:px-6 md:px-8 flex justify-start items-center h-screen">
+        <div className="text-left">
+          <p className="text-gray-400 text-lg sm:text-2xl sm:leading-10 font-medium mb-10 sm:mb-11">
+            Hi! I am
           </p>
-          <div className="flex flex-wrap justify-center space-y-4 sm:space-y-0 sm:space-x-4 ">
+          <h1 className="text-4xl sm:text-6xl lg:text-7xl leading-none font-extrabold tracking-tight text-white-900 my-8 sm:mt-14 sm:mb-10">
+            Antony Budianto
+          </h1>
+          <p className="text-gray-400 text-lg sm:text-2xl sm:leading-10 font-medium mb-10 sm:mb-11">
+            Engineer, Open-source dev, Google Cloud certified
+          </p>
+          <div className="flex flex-wrap space-y-4 sm:space-y-0 sm:space-x-4 ">
             <a
-              className="w-full sm:w-auto flex-none bg-gray-900 hover:bg-gray-700 text-white text-lg leading-6 font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200"
-              href="/docs"
+              className="w-full sm:w-auto flex-none bg-gray-700 hover:bg-gray-600 text-white text-lg leading-6 font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200"
+              href="https://github.com/antonybudianto"
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              Get started
+              GitHub
             </a>
             <a
-              className="w-full sm:w-auto flex-none bg-pink-400 text-white text-lg leading-6 font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200"
-              href="/docs"
+              className="w-full sm:w-auto flex-none bg-blue-600 hover:bg-blue-900 text-white text-lg leading-6 font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200"
+              href="#work"
             >
-              View demo
+              View work
             </a>
           </div>
         </div>
+      </div>
+      <div className="px-4 py-8 mx-auto container">
+        <h2
+          id="work"
+          className="mb-5 text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-green-700 to-blue-700"
+        >
+          My recent work
+        </h2>
+        <div className="flex flex-col lg:flex-row">
+          <div className="px-4 py-8 sm:px-6 md:px-8 bg-white text-gray-500 rounded shadow-md">
+            <h3 className="text-2xl">Deesain</h3>
+            <p className="mb-5">
+              Modern Design platform for social media, thumbnails, or any
+              creative works. Made using NextJS (React), Bootstrap, and
+              Firebase.{" "}
+              <a
+                className="text-blue-500"
+                href="https://deesain.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                https://deesain.com
+              </a>
+            </p>
+            <Image
+              src="/deesain.jpg"
+              alt="Picture of the author"
+              className="rounded"
+              width={2880 / 5}
+              height={1446 / 5}
+            />
+          </div>
+
+          <div className="px-4 py-8 sm:px-6 md:px-8 bg-white text-gray-500 rounded shadow-md mt-2 lg:mt-0 lg:ml-5">
+            <h3 className="text-2xl">StickyNoted</h3>
+            <p className="mb-5">
+              Simple Sticky Note app with Markdown flavour. Supports progressive
+              web app so users can install on Android and iOS.{" "}
+              <a
+                className="text-blue-500"
+                href="https://stickynoted.xyz"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                https://stickynoted.xyz
+              </a>
+            </p>
+            <Image
+              src="/sticky.jpeg"
+              alt="StickyNoted"
+              className="rounded"
+              width={2880 / 5}
+              height={1446 / 5}
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="px-4 py-8 mx-auto text-center container text-gray-400">
+        Designed with{" "}
+        <a
+          href="https://tailwindcss.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          TailwindCSS
+        </a>
+        . {new Date().getFullYear()}.
       </div>
     </div>
   );
