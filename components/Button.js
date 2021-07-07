417d026918cf360c74a802195657d2b2299c2f72
Antony <antony.budianto@tokopedia.com>
2021-07-07 22:28:54 +0700

    try fix purge tailwind

--- components/Button.js
+++ components/Button.js
@@ -1,11 +1,11 @@
 const Button = ({ dark, children, className = "", ...p }) => (
   <button
     {...p}
-    className={`w-auto shadow-md flex-none bg-${
-      dark ? "gray-700" : "white"
-    } hover:bg-${dark ? "gray-600" : "blue-100"} ${
-      dark ? "text-white" : "text-gray-900"
-    } text-lg ml-2 leading-6 font-semibold py-2 px-5 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-${
+    className={`w-auto shadow-md flex-none ${dark ? "bg-gray-700" : "bg-white"} 
+    ${dark ? "hover:bg-gray-600" : "hover:bg-blue-100"}
+     ${
+       dark ? "text-white" : "text-gray-900"
+     } text-lg ml-2 leading-6 font-semibold py-2 px-5 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-${
       dark ? "gray-900" : "blue-300"
     } focus:outline-none transition-colors duration-200 ${className}`}
   >
