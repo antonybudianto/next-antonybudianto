608e9d4923db7cefb77ee5c0615a660f0317a62f
Antony <antony.budianto@tokopedia.com>
2021-07-07 22:09:00 +0700

    new landing design

--- pages/index.js
+++ pages/index.js
@@ -1,193 +1,123 @@
 import Head from "next/head";
 import Image from "next/image";
-import Link from "next/link";
 
 import deesainJpg from "../public/deesain.jpg";
 import stickyJpg from "../public/sticky.jpeg";
 import jsbJpg from "../public/jsb.jpg";
 import apart3d from "../public/meta-home.jpg";
+import HomePortfolio from "../components/HomePortfolio";
 
 const WORKS = [
-  [
-    {
-      title: "Deesain",
-      href: "https://deesain.com",
-      imgsrc: deesainJpg,
-      p: `Modern Design platform for social media, thumbnails, or any
-  creative works. Made using NextJS (React), Bootstrap, and
-  Firebase.`,
-      w: 2880 / 5,
-      h: 1446 / 5,
-      gradient: "from-gray-800 to-gray-700",
-    },
-    {
-      title: "StickyNoted",
-      href: "https://stickynoted.xyz",
-      imgsrc: stickyJpg,
-      p: `Simple Sticky Note app with Markdown flavour. Supports progressive
-      web app so users can install on Android and iOS.`,
-      w: 2880 / 5,
-      h: 1446 / 5,
-      gradient: "from-red-700 to-yellow-600",
-    },
-    {
-      title: "JSBench",
-      href: "https://jsbench.netlify.com",
-      imgsrc: jsbJpg,
-      p: `Simple JS Benchmark tools, easily compare codes side-by-side.
-      Using WebWorker API for UI responsiveness.`,
-      w: 2184 / 7,
-      h: 1278 / 7,
-      imgborder: true,
-      gradient: "from-gray-900 to-blue-900",
-    },
-  ],
+  {
+    title: "3D Apart",
+    localHref: "/home",
+    imgSrc: apart3d,
+    desc: `Interactive Web 3D Apartment with low-poly design.`,
+  },
+  {
+    title: "Deesain",
+    href: "https://deesain.com",
+    imgSrc: deesainJpg,
+    desc: `Modern Design platform for social media, thumbnails, or any
+  creative works`,
+    imgW: 2880 / 5,
+    imgH: 1446 / 5,
+  },
+  {
+    title: "StickyNoted",
+    href: "https://stickynoted.netlify.app/",
+    imgSrc: stickyJpg,
+    desc: `Simple Sticky Note app with Markdown flavour. Supports progressive
+      web app so users can install on Android, iOS, Windows.`,
+    imgW: 2880 / 5,
+    imgH: 1446 / 5,
+  },
+  {
+    title: "JSBench",
+    href: "https://jsbench.netlify.com",
+    imgSrc: jsbJpg,
+    desc: `Simple JS Benchmark tools, easily compare code's performance side-by-side.`,
+    imgW: 2184 / 4,
+    imgH: 1278 / 4,
+  },
 ];
 
 export default function Home() {
   return (
-    <div className="bg-gradient-to-br from-gray-900 via-gray-800 to-gray-800 text-white h-screen">
-      <Head>
-        <title>Antony Budianto</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Engineer, Open-source dev, Google cloud certified"
-        />
-        <meta name="og:title" content="Antony Budianto" />
-        <meta
-          name="og:description"
-          content="Engineer, Open-source dev, Google cloud certified"
-        />
-      </Head>
-
-      <div
-        className="container mx-auto px-4 sm:px-6 md:px-8 flex justify-start items-center"
-        style={{
-          height: "95%",
-        }}
-      >
-        <div className="text-left">
-          <div className="container">
-            <Image
-              src="/profile.jpeg"
-              alt="Profile"
-              priority
-              className="rounded-full mb-5"
-              width={150}
-              height={150}
-            />
-          </div>
-          <h1 className="text-4xl sm:text-6xl lg:text-7xl leading-none font-extrabold tracking-tight text-white-900 my-8 sm:mt-14 sm:mb-10">
-            Antony Budianto
-          </h1>
-          <p className="text-gray-400 text-lg sm:text-2xl sm:leading-10 font-medium mb-10 sm:mb-11">
-            Engineer, Open-source dev, Google Cloud certified
-          </p>
-          <div className="flex flex-wrap space-y-4 sm:space-y-0 sm:space-x-4 ">
-            <a
-              className="w-full sm:w-auto flex-none bg-gray-700 hover:bg-gray-600 text-white text-lg leading-6 font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200"
-              href="https://github.com/antonybudianto"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              GitHub
-            </a>
-            <a
-              className="w-full sm:w-auto flex-none bg-blue-600 hover:bg-blue-900 text-white text-lg leading-6 font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200"
-              href="#work"
-            >
-              View work
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className="p-8 mx-auto container rounded-xl shadow-md bg-white">
-        <h2
-          id="work"
-          className="mb-8 text-3xl lg:text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-green-700 to-blue-700"
+    <>
+      <div className="bg-gradient-to-br from-gray-900 via-gray-800 to-gray-800 text-white h-screen">
+        <Head>
+          <title>Antony Budianto</title>
+          <link rel="icon" href="/favicon.ico" />
+          <meta
+            name="description"
+            content="Engineer, Open-source dev, Google cloud certified"
+          />
+          <meta name="og:title" content="Antony Budianto" />
+          <meta
+            name="og:description"
+            content="Engineer, Open-source dev, Google cloud certified"
+          />
+        </Head>
+        <div
+          className="container mx-auto px-4 sm:px-6 md:px-8 flex justify-start items-center"
+          style={{
+            height: "95%",
+          }}
         >
-          My recent work
-        </h2>
-        <div className="flex flex-col items-center lg:flex-row py-2 bg-white text-gray-500 mb-8 lg:w-12/12 lg:mb-14 lg:mb-0">
-          <div className="flex-grow">
-            <h2 className="lg:text-3xl text-2xl font-bold mb-2 lg:mb-3">
-              <Link href="/home">
-                <a className="text-green-800 hover:underline">3D Apart</a>
-              </Link>
-            </h2>
-            <p className="mb-5 lg:text-xl">
-              Interactive 3D Web with simple apartment concept and low-poly
-              design. Made with React and Three.js. <br />
-              You can enjoy 3D experience with pan, swipe, zoom, and Night mode!{" "}
-              <br />
+          <div className="text-left">
+            <div className="container">
+              <Image
+                src="/profile.jpeg"
+                alt="Profile"
+                priority
+                className="rounded-full mb-5"
+                width={150}
+                height={150}
+              />
+            </div>
+            <h1 className="text-4xl sm:text-6xl lg:text-7xl leading-none font-extrabold tracking-tight text-white-900 my-8 sm:mt-14 sm:mb-10">
+              Antony Budianto
+            </h1>
+            <p className="text-gray-400 text-lg sm:text-2xl sm:leading-10 font-medium mb-10 sm:mb-11">
+              Engineer, Open-source dev, Google Cloud certified
             </p>
-            <small className="text-sm">
-              💡 Tips: Try clicking the door and bed lamp!
-            </small>
-          </div>
-          <div className="rounded mt-5 sm:mt-8 lg:ml-20">
-            <Image
-              src={apart3d}
-              alt={""}
-              className="rounded"
-              placeholder="blur"
-            />
+            <div className="flex flex-wrap space-y-4 sm:space-y-0 sm:space-x-4 ">
+              <a
+                className="w-full sm:w-auto flex-none bg-gray-700 hover:bg-gray-600 text-white text-lg leading-6 font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200"
+                href="https://github.com/antonybudianto"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </a>
+              <a
+                className="w-full sm:w-auto flex-none bg-blue-600 hover:bg-blue-900 text-white text-lg leading-6 font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200"
+                href="#work"
+              >
+                View work
+              </a>
+            </div>
           </div>
         </div>
-        {WORKS.map((row, i) => (
-          <div
-            key={i}
-            className="flex flex-col lg:flex-row"
-            style={{
-              contentVisibility: "auto",
-            }}
-          >
-            {row.map((r, ri) => (
-              <div
-                key={ri}
-                className={`py-2 bg-white text-gray-500 lg:w-4/12 ${
-                  ri !== 0 ? "mt-8 lg:mt-0 lg:ml-12" : ""
-                }`}
-              >
-                <h3 className="text-2xl font-bold mb-2">
-                  <a
-                    className="text-green-800 hover:underline"
-                    href={r.href}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {r.title}
-                  </a>
-                </h3>
-                <p className="mb-5">{r.p}</p>
-                <div className={r.imgborder ? "border rounded" : ""}>
-                  <Image
-                    src={r.imgsrc}
-                    alt={r.title}
-                    className="rounded"
-                    placeholder="blur"
-                    width={r.w}
-                    height={r.h}
-                  />
-                </div>
-              </div>
-            ))}
-          </div>
-        ))}
       </div>
+      {WORKS.map((w, i) => (
+        <HomePortfolio
+          key={i}
+          localHref={w.localHref}
+          href={w.href}
+          title={w.title}
+          desc={w.desc}
+          imgSrc={w.imgSrc}
+          imgAlt={w.title}
+          imgW={w.imgW}
+          imgH={w.imgH}
+        />
+      ))}
 
-      <div className="px-4 py-8 mx-auto text-center container text-gray-500">
-        Designed with{" "}
-        <a
-          href="https://tailwindcss.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          TailwindCSS
-        </a>
-        . {new Date().getFullYear()}.
+      <div className="px-4 py-8 bg-gray-900 mx-auto text-center text-gray-100">
+        &copy; {new Date().getFullYear()}. Antony Budianto.
       </div>
-    </div>
+    </>
   );
 }
