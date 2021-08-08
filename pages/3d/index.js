07501c85eeb5ee0489780b7bfa301c35e88ef224
Antony <antony.budianto@tokopedia.com>
2021-08-08 15:51:51 +0700

    add missing meta

--- pages/3d/index.js
+++ pages/3d/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Link from "next/link";
 
 const LIST = [
@@ -17,6 +18,43 @@ export default function MaldiveMiniPage() {
   return (
     <div className="bg-gradient-to-br from-blue-100 to-blue-50 dark:from-gray-900 dark:via-gray-800 dark:to-gray-800 text-gray-800 dark:text-white lg:h-screen">
       <div className="container mx-auto px-4 sm:px-6 md:px-8 py-16 lg:py-10 ">
+        <Head>
+          <title>3D Showcase by Antony</title>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          <meta
+            name="description"
+            content="3D Interactive Showcase by Antony Budianto"
+          />
+          <meta property="og:site_name" content="antonybudianto.com" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="3D Showcase" />
+          <meta
+            property="og:description"
+            content="3D Interactive Showcase by Antony Budianto"
+          />
+          <meta property="og:url" content="https://antonybudianto.com/3d" />
+          <meta
+            property="og:image"
+            content="https://antonybudianto.com/meta-maldive-mini.jpg"
+          />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="3D Showcase" />
+          <meta
+            name="twitter:description"
+            content="3D Interactive Showcase by Antony Budianto"
+          />
+          <meta name="twitter:url" content="https://antonybudianto.com/3d" />
+          <meta
+            name="twitter:image"
+            content="https://antonybudianto.com/meta-maldive-mini.jpg"
+          />
+          <meta name="twitter:site" content="@antonybudianto" />
+          <meta name="theme-color" content="#FFFFFF" />
+        </Head>
         <div className="text-4xl">3D Showcase</div>
         <div>
           by{" "}
--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,11 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap"
             rel="stylesheet"
